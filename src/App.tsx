9946b4bcaeb68a9d4825f02773fdd3db974f0692
Eunock-web <izumishinishi7@gmail.com>
2025-12-04 07:58:25 +0100

    Mis en place du card pour le detail des voyages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import './App.css'
+import Card from './components/Utils/Card'
 
 function App() {
 
   return (
     <>
-      
+      <div className=''>
+
+      <div className=' grid grid-cols-3 '>
+          <div>
+          <Card voyageTitre=' Envole ' voyageDescription='Voyage pour Bamako' voyageImage=' src\images\One Piece - manga wallpaper.jfif ' voyagePrix={1500} />
+          </div>
+
+          <div>
+            <Card voyageTitre=' Envole ' voyageDescription='Voyage pour Bamako' voyageImage=' src\images\One Piece - manga wallpaper.jfif ' voyagePrix={1500} />
+          </div>
+
+          <div>
+            <Card voyageTitre=' Envole ' voyageDescription='Voyage pour Bamako' voyageImage=' src\images\One Piece - manga wallpaper.jfif ' voyagePrix={1500} />
+          </div>
+          
+        </div>
+
+        
+      </div>
     </>
   )
 }
--- src/components/Utils/Card.tsx
+++ src/components/Utils/Card.tsx
@@ -1,31 +1,33 @@
 import Button from "./Button";
 
 interface CardInterface{
-    voyageImage: string,
-    voyageTitre: string,
-    voyageDescription: string,
-    voyagePrix: number
+    voyageImage?: string,
+    voyageTitre?: string,
+    voyageDescription?: string,
+    voyagePrix?: number
 }
 
 function Card( {voyageTitre, voyageDescription, voyageImage, voyagePrix} : CardInterface ) {
     return (
         <>
-            <div>
-                <img src={ voyageImage } alt="" />
-            </div>
-            <div className=" flex flex-col gap-3 ">
-                <div className=" flex flex-col gap-2 ">
-                    <h1> { voyageTitre } </h1>
-                    <p> { voyageDescription } </p>
+            <div className=" border border-gray-400 rounded-xl ">
+                <div className=" border-b  hover:cursor-pointer  ">
+                    <img src={voyageImage} alt="" className=" w-[25vw] h-[40vh] aspect-square object-cover border border-neutral rounded-sm " />
                 </div>
-
-                <div className=" flex flex-col ">
-                    <div className=" flex flex-row gap-1 ">
-
+                <div className=" flex flex-col gap-3 p-3 ">
+                    <div className=" flex flex-col gap-2 ">
+                        <h1 className=" text-xl  "> {voyageTitre} </h1>
+                        <p> {voyageDescription} </p>
                     </div>
-                    <div className=" flex flex-row justify-between">
-                        <p> <span className=""> $ { voyagePrix } </span> <span className=""> /person </span> </p>
-                        <Button className={ "bg-blue-500 p-2 " } > View Details </Button>
+
+                    <div className=" flex flex-col ">
+                        <div className=" flex flex-row gap-1 ">
+
+                        </div>
+                        <div className=" flex flex-row justify-between">
+                            <p> <span className=""> $ {voyagePrix} </span> <span className=""> /person </span> </p>
+                            <Button className="" > View Details </Button>
+                        </div>
                     </div>
                 </div>
             </div>
