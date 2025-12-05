ee609e18e10ab7701ac6c09b7d5099502f1c0283
Eunock <izumishinishi7@gmail.com>
2025-12-05 01:21:24 +0100

    Finalisation complete du front de la page Home

--- src/components/pages/Home/Main.tsx
+++ src/components/pages/Home/Main.tsx
@@ -1,6 +1,33 @@
 import Card from "../../Utils/Card";
 import { Star } from "lucide-react";
 
+const voyagesCulturels = [
+  {
+    voyageTitre: "Palais Royaux d'Abomey",
+    voyageDescription: "Découvrez l'histoire fascinante du royaume du Dahomey à travers ses palais historiques classés au patrimoine mondial de l'UNESCO.",
+    voyagePrix: 45000,
+    voyageImage: "src/images/One Piece - manga wallpaper.jfif",
+    rang: <Star size={ 20 } className=" text-blue-800 " />,
+    nombreRang: 127
+  },
+  {
+    voyageTitre: "Cité Lacustre de Ganvié",
+    voyageDescription: "Explorez la Venise de l'Afrique, un village entièrement construit sur pilotis au milieu du lac Nokoué.",
+    voyagePrix: 35000,
+    voyageImage: "src/images/One Piece - manga wallpaper.jfif",
+    rang: <Star size={ 20 } className=" text-blue-800 " />,
+    nombreRang: 203
+  },
+  {
+    voyageTitre: "Temple des Pythons à Ouidah",
+    voyageDescription: "Vivez une expérience unique dans ce temple sacré abritant des dizaines de pythons royaux vénérés.",
+    voyagePrix: 25000,
+    voyageImage: "src/images/One Piece - manga wallpaper.jfif",
+    rang: <Star size={ 20 } className=" text-blue-800 " />,
+    nombreRang: 156
+  }
+];
+
 function Main() {
     return (
         <div className="max-w-8xl mx-auto p-4 md:p-8 lg:grid lg:grid-cols-4 lg:gap-8">
@@ -51,12 +78,12 @@ function Main() {
             <section className="lg:col-span-3"> 
                 <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
                     
-                    <Card voyageTitre="Ouidah History Tour" voyageDescription="Discover the poignant history of the slave route and vibriant Voodoo culture" voyagePrix={450} voyageImage="src/images/One Piece - manga wallpaper.jfif" />
-                    <Card voyageTitre="Ouidah History Tour" voyageDescription="Discover the poignant history of the slave route and vibriant Voodoo culture" voyagePrix={450} voyageImage="src/images/One Piece - manga wallpaper.jfif" />
-                    <Card voyageTitre="Ouidah History Tour" voyageDescription="Discover the poignant history of the slave route and vibriant Voodoo culture" voyagePrix={450} voyageImage="src/images/One Piece - manga wallpaper.jfif" />
-                    <Card voyageTitre="Ouidah History Tour" voyageDescription="Discover the poignant history of the slave route and vibriant Voodoo culture" voyagePrix={450} voyageImage="src/images/One Piece - manga wallpaper.jfif" />
-                    <Card voyageTitre="Ouidah History Tour" voyageDescription="Discover the poignant history of the slave route and vibriant Voodoo culture" voyagePrix={450} voyageImage="src/images/One Piece - manga wallpaper.jfif" />                    
-                    <Card voyageTitre="Ouidah History Tour" voyageDescription="Discover the poignant history of the slave route and vibriant Voodoo culture" voyagePrix={450} voyageImage="src/images/One Piece - manga wallpaper.jfif" />                    
+                    {
+                        voyagesCulturels.map((elts,index) =>
+                            <Card voyageTitre= { elts.voyageTitre} voyageDescription={elts.voyageDescription} voyagePrix={elts.voyagePrix} voyageImage={elts.voyageImage} key={index} />                    
+                        )
+                    }
+
                 </div>
             </section>
         </div>
